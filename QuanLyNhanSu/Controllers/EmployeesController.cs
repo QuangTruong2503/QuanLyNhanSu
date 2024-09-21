8d28059aea332dce81396502dface8bd831c4254
Trần Vũ Quang Trường <trutruong2503@gmail.com>
2024-09-21 11:58:16 +0700

    create view employees

---
+++ QuanLyNhanSu/Controllers/EmployeesController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuanLyNhanSu.Data;
+
+namespace QuanLyNhanSu.Controllers
+{
+    public class EmployeesController : Controller
+    {
+
+        private QuanLyNhanSuDbContext _context;
+        public EmployeesController(QuanLyNhanSuDbContext context)
+        {
+            _context = context;
+        }
+        // GET: EmployeesController
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _context.employees.ToListAsync();
+            return View(employees);
+        }
+
+        // GET: EmployeesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EmployeesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ QuanLyNhanSu/Models/EmployeesModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuanLyNhanSu.Models
+{
+    public class EmployeesModel
+    {
+        [Key]
+        [Display(Name = "Mã nhân viên")]
+        public int employee_id { get; set; }
+        [Display(Name = "Họ và đệm")]
+        public required string first_name { get; set; }
+        [Display(Name = "Tên")]
+        public required string last_name { get; set; }
+        [Display(Name = "Email")]
+        public required string email { get; set; }
+        [Display(Name = "SĐT")]
+        public required string phone { get; set; }
+        [Display(Name = "Ngày sinh")]
+        public DateOnly? date_of_birth { get; set; }
+        public enum Gender
+        {
+            Nam,
+            Nữ,
+            Khác
+        }
+        [Display(Name = "Giới tính")]
+        [Column(TypeName = "varchar(100)")] // MySQL sẽ lưu enum dưới dạng chuỗi
+        public Gender gender { get; set; }
+        [Display(Name = "Ngày bắt đầu")]
+        public DateOnly hire_date { get; set; }
+        [Display(Name = "Vị trí")]
+        public required string position { get; set; }
+        [Display(Name = "Mã phòng ban")]
+        public int department_id { get; set; } 
+    }
+}
