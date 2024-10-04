6dbd24e58f7d50c9548b7b4be0e15d8b18a20194
Trần Vũ Quang Trường <trutruong2503@gmail.com>
2024-10-04 11:04:45 +0700

    update layout

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,25 +1,16 @@
 ﻿using System;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Newtonsoft.Json;
 using QuanLyNhanSu.Helpers;
+
 class Program
 {
-    public class Questions
-    {
-        public string question_text { get; set; }
-        public List<string> answers { get; set; }
-        public string correct_answer { get; set; }
-    }
-
     static void Main(string[] args)
     {
-        var currentDate = DateTime.Now; 
-        var day = currentDate.Day;
-        var month = currentDate.Month;
-        var year = currentDate.Year;
-        var password = $"{day}{month}{year}";
-        Console.WriteLine(password);
         
-
     }
 
 }
