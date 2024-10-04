df386d94ae02c6fa764f5b154158f04e6ce1e408
Trần Vũ Quang Trường <trutruong2503@gmail.com>
2024-10-04 11:04:34 +0700

    add ChangePassword

---
+++ QuanLyNhanSu/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace QuanLyNhanSu.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        public required string CurrentPassword { get; set; }
+        public required string NewPassword { get; set; }
+        public required string ConfirmPassword { get; set; }
+    }
+}
